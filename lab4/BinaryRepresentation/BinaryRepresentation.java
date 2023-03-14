280719292e5a0789ecb0cc16973909542ae1d7aa
booleto <exaccxd@gmail.com>
2023-03-14 22:18:02 +0700

    update

--- lab4/BinaryRepresentation/BinaryRepresentation.java
+++ lab4/BinaryRepresentation/BinaryRepresentation.java
@@ -1,9 +1,23 @@
 package BinaryRepresentation;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+
 public class BinaryRepresentation {
-    public static void main(String[] args) {
-        int n = 238423478;
-        System.out.println(bin(n));
+    public static void main(String[] args) throws IOException{
+        FileWriter logfile = new FileWriter("log_bin.txt");;
+        BufferedWriter log = new BufferedWriter(logfile);;
+        long start, end;
+        
+        for (int i = 1; i < 10000000; i+= 1000) {
+            start = System.nanoTime();
+            System.out.println(bin(i));
+            end = System.nanoTime();
+            log.write(Long.toString(end - start));
+            log.newLine();
+        }
+        log.flush();
     }
 
     static String bin(int n) {
--- lab4/PrimeFactorization/PrimeFactorization.java
+++ lab4/PrimeFactorization/PrimeFactorization.java
@@ -1,12 +1,26 @@
 package PrimeFactorization;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+
 /**
  * PrimeFactorization
  */
 public class PrimeFactorization {
-    public static void main(String[] args) {
-        int n = 1090518034;
-        System.out.println(fact(n));
+    public static void main(String[] args) throws IOException {
+        FileWriter logfile = new FileWriter("log_fact.txt");;
+        BufferedWriter log = new BufferedWriter(logfile);;
+        long start, end;
+        
+        for (int i = 1; i < 10000000; i+= 1000) {
+            start = System.nanoTime();
+            System.out.println(fact(i));
+            end = System.nanoTime();
+            log.write(Long.toString(end - start));
+            log.newLine();
+        }
+        log.flush();
     }
 
     static String fact(int n) {
